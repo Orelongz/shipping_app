f2afc8987cff2963980a7ef7082288cc3c9b0e0f
Pelumi Longe <longe.pelumi@yahoo.com>
2026-02-03 20:13:13 +0100

    - fix rubocop lint issues - run test with rspec

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # Custom health endpoint with database connectivity check
   get "health" => "health#show"
 
-  resources :invoices, only: [:show] do
+  resources :invoices, only: [ :show ] do
     get :overdue, on: :collection
     post :generate, on: :collection
   end
